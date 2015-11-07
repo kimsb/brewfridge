cf9a0990d348c5009abd52db45d608656d895285
staurz <emil.staurset@gmail.com>
2015-11-07 17:34:29 +0100

    Improve error handling on PG connection

--- server/temperatureRepository.js
+++ server/temperatureRepository.js
@@ -2,7 +2,10 @@ var pg = require('pg');
 
 function insertTemp(temp) {
   pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (err) throw err;
+    if(handleError(err)){
+      console.log("error i pg connection");
+      return;
+    }
 
     client.query('INSERT INTO temperature (date_measured, temp)' +
       'VALUES ($1,$2) RETURNING id', [new Date(), temp],
@@ -19,8 +22,9 @@ function insertTemp(temp) {
 function getTemp(response) {
   console.log("get Temp i repo kallt");
   pg.connect(process.env.DATABASE_URL, function(err, client) {
-        if (err) {
-          console.log("error pa getTemp")
+        if(handleError(err,client)) {
+            console.log("error i pg connection");
+            return;
         }
 
         var query = client.query('SELECT date_measured, temp FROM temperature');
@@ -36,5 +40,21 @@ function getTemp(response) {
         });
       }
 
+  function handleError(error, client) {
+    // no error occurred, continue with the request
+     if(!err) return false;
+
+     // An error occurred, remove the client from the connection pool.
+     // A truthy value passed to done will remove the connection from the pool
+     // instead of simply returning it to be reused.
+     // In this case, if we have successfully received a client (truthy)
+     // then it will be removed from the pool.
+     if(client){
+       done(client);
+     }
+     res.writeHead(500, {'content-type': 'text/plain'});
+     res.end('An error occurred');
+     return true;
+  }
       module.exports.insertTemp = insertTemp;
       module.exports.getTemp = getTemp;
