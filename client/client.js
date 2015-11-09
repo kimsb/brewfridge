5252a1d24eef8e687a49ad685d7ceef161edb9cf
staurz <emil.staurset@gmail.com>
2015-11-09 20:16:44 +0100

    Change to Ajax with JSONP instead of regular ajax

--- client/client.js
+++ client/client.js
@@ -1,7 +1,8 @@
 //var url = "https://brew-fridge.herokuapp.com";
 //var url = "http://localhost:5000";
-var url ="http://brew-fridge.azurewebsites.net"
+var url = "http://brew-fridge.azurewebsites.net"
 var graph = require('./graph.js');
+
 function start() {
   $.post(url + "/start", function(data) {});
 }
@@ -11,15 +12,25 @@ function stop() {
 }
 
 $(document).ready(function() {
-  var data = {
-    on: true
-  };
+  $.ajax({
+    url: "/data",
+    dataType: "jsonp",
+    success: graph.buildGraph
+  });
+  //$.get(url + "/data", graph.buildGraph);
 
-  $.get(url + "/data", graph.buildGraph);
 
-  $.get(url + "/currentTemp", function(data) {
-    var status = data.isOn ? "på" : "av";
-    $(".result").html(data.temp + "\xB0 C er temperaturen nå og kjøleskapet er " + status);
+  // $.get(url + "/currentTemp", function(data) {
+  //   var status = data.isOn ? "på" : "av";
+  //   $(".result").html(data.temp + "\xB0 C er temperaturen nå og kjøleskapet er " + status);
+  // });
+  $.ajax({
+    url: "/data",
+    dataType: "jsonp",
+    success: function (data) {
+      var status = data.isOn ? "på" : "av";
+      $(".result").html(data.temp + "\xB0 C er temperaturen nå og kjøleskapet er " + status);
+    },
   });
 
   $('.startbutton').click(start);
