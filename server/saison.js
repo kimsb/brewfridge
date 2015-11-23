f234b665b5ef6ac62a227ce519e2028e3aa33f3e
staurz <emil.staurset@gmail.com>
2015-11-23 19:17:01 +0100

    Less variance for the saison program

--- server/saison.js
+++ server/saison.js
@@ -3,19 +3,28 @@ var tellstick = require('./tellstick.js');
 var lastTemp;
 
 var target = 30.0;
+var lowThresh = 29.75
+var highThresh =30.10
 
 function controlTemperature(temp){
+  var deltaTemp = temp-lastTemp;
 
-    if(temp < target) {
-      tellstick.startHeater();
-      tellstick.stopFridge();
-      console.log("starter varmeovn");
-    }
-    else if(temp > target) {
-      tellstick.stopHeater();
-      tellstick.stopFridge();
-      console.log("stopper varmeovn");
-    }
+  if(temp > lowThresh && deltaTemp > 0 ) {
+    console.log("Temperatur er: " + temp + " og deltaTemp er: " + deltaTemp +". Stopper varmeovn og starter kjøleskap")
+    tellstick.stopHeater();
+  }
+  if(temp < highThresh && deltaTemp < 0){
+    console.log("Temperatur er: " + temp + " og deltaTemp er: " + deltaTemp +". Starter varmeovn")
+    tellstick.startHeater();
+  }
+  if(temp < lowThresh){
+    console.log("Temperatur er: " + temp + " og deltaTemp er: " + deltaTemp +". Starter varmeovn stopper kjøleskap")
+    tellstick.startHeater();
+  }
+  if(temp > highThresh) {
+    tellstick.stopHeater();
+  }
+  lastTemp = temp;
 }
 
 module.exports.controlTemperature = controlTemperature;
