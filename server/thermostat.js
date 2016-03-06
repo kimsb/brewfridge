31ea6318807559e422c3c0012954dc105a2d8c91
kimsb <kim.stephen.bovim@bekk.no>
2016-03-06 21:31:13 +0100

    Revert: fjerner buffer på 0.1C - kanskje bedre uten...

--- server/thermostat.js
+++ server/thermostat.js
@@ -4,17 +4,17 @@ var lastTemp;
 var currentTemp = 0;
 
 var target = 12.0;
-var buffer = 0.1;
+
 
 fridgeHighThresh = 19.35
 function controlTemperature(temp){
     var deltaTemp = temp-lastTemp;
 
-    if(temp > target + buffer) {
+    if(temp > target ) {
       console.log("Temperatur er: " + temp + " starter kjøleskap")
       tellstick.startFridge();
     }
-    if(temp < target + buffer){
+    if(temp < target){
       console.log("Temperatur er: " + temp + " stopper kjøleskap")
       tellstick.stopFridge();
     }
