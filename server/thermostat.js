0e18b20195e26384a3ca46539f96f1fcd63e084a
kimsb <kim.stephen.bovim@bekk.no>
2016-06-21 10:30:42 +0200

    skrur av kjøleskapet om det er likt måltemp
    nedkjøling er mer effektiv enn oppvarming, så på denne måten vil man komme nærmere måltemp.

--- server/thermostat.js
+++ server/thermostat.js
@@ -1,22 +1,16 @@
 var tellstick = require('./tellstick.js');
 
-var lastTemp;
-var currentTemp = 0;
-
 var target = 20.0;
 
-
-fridgeHighThresh = 19.35
 function controlTemperature(temp){
-    var deltaTemp = temp-lastTemp;
 
     if(temp > target ) {
-      console.log("Temperatur er: " + temp + " starter kjøleskap")
-      tellstick.startFridge();
+        console.log("Temperatur er: " + temp + " starter kjøleskap");
+        tellstick.startFridge();
     }
-    if(temp < target){
-      console.log("Temperatur er: " + temp + " stopper kjøleskap")
-      tellstick.stopFridge();
+    if(temp <= target) {
+        console.log("Temperatur er: " + temp + " stopper kjøleskap");
+        tellstick.stopFridge();
     }
 
 }
